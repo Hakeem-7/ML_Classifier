cfb3848a07c13a7e32fbd9281f933563a3e3a0d3
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2020-03-22 01:13:36 -0500

    Scripting done!

--- ML Script.R
+++ ML Script.R
@@ -146,11 +146,63 @@ pred_table
 
 
 
+
 #Perceptron Algorithm
 
+# write function that takes in the data frame, learning rate - eta, and number of epochs - n.iter and updates the weight factor. 
+# To obtain the final weight and the number of epochs required for the weight to converge
 
+#Here we separate the attributes from the class
 
+x <- a4a_train[-1] 
+names(x) <- tolower(names(x))
 
+# create species labels
+y <- train1$Label   
+
+perceptron <- function(x, y, eta, n_iter) {
+  
+  # initialize weight vector
+  weight <- rep(0, dim(x)[2] + 1)
+  errors <- rep(0, n_iter)
+  
+  
+  # loop over number of epochs niter
+  for (jj in 1:n_iter) {
+    
+    # loop through training data set
+    for (ii in 1:length(y)) {
+      
+      # Predict binary label using Heaviside activation 
+      # function
+      z <- sum(weight[2:length(weight)] * 
+                 as.numeric(x[ii,])) + weight[1]
+      if(z < 0) {
+        y_pred <- -1
+      } else {
+        y_pred <- 1
+      }
+      
+      # Change weight - the formula doesn't do anything 
+      # if the predicted value is correct
+      weight_diff <- eta * (y[ii] - y_pred) * 
+        c(1, as.numeric(x[ii, ]))
+      weight <- weight + weight_diff
+      
+      # Update error function
+      if ((y[ii] - y_pred) != 0.0) {
+        errors[jj] <- errors[jj] + 1
+      }
+      
+    }
+  }
+  
+  # weight to decide between the two species 
+  print(weight)
+  return(errors)
+}
+
+err <- perceptron(x,y,0.9,10)
 
 
 
@@ -228,8 +280,112 @@ pred_table1
 
 #Perceptron Algorithm
 
+summary(iris)
+#create sub-dataframe
+
+iris_subdf1 <- iris[1:100, c(1,3,5)]
+names(iris_subdf1)
+
+library(ggplot2)
+ggplot(iris_subdf1, aes(x = iris_subdf1$SepalLength..cm., y = iris_subdf1$PetalLength..cm.)) + 
+  geom_point(aes(colour=iris_subdf1$Species, shape = iris_subdf1$Species), size = 3) +
+  xlab("sepal length") + 
+  ylab("Petal width") + 
+  ggtitle("Iris Species")   #The data looks linearly separable
 
 
+
+iris_subdf1[, 4] <- 1     #initialize
+iris_subdf1[iris_subdf1[, 3] == "Iris-setosa", 4] <- -1  #setosa is now -1
+
+x <- iris_subdf1[, c(1, 2)]    #attributes
+y <- iris_subdf1[, 4]          #class values
+tail(y)
+
+
+# write function that takes in the data frame, learning rate - eta, and number of epochs - n.iter and updates the weight factor. 
+# To obtain the final weight and the number of epochs required for the weight to converge
+
+#Here we separate the attributes from the class
+
+perceptron <- function(x, y, eta, n_iter) {
+  
+  # initialize weight vector
+  weight <- rep(0, dim(x)[2] + 1)
+  errors <- rep(0, n_iter)
+  
+  
+  # loop over number of epochs niter
+  for (jj in 1:n_iter) {
+    
+    # loop through training data set
+    for (ii in 1:length(y)) {
+      
+      # Predict binary label using Heaviside activation 
+      # function
+      z <- sum(weight[2:length(weight)] * 
+                 as.numeric(x[ii,])) + weight[1]
+      if(z < 0) {
+        y_pred <- -1
+      } else {
+        y_pred <- 1
+      }
+      
+  # Change weight - the formula doesn't do anything 
+  # if the predicted value is correct
+      weight_diff <- eta * (y[ii] - y_pred) * 
+        c(1, as.numeric(x[ii, ]))
+      weight <- weight + weight_diff
+      
+      # Update error function
+      if ((y[ii] - y_pred) != 0.0) {
+        errors[jj] <- errors[jj] + 1
+      }
+      
+    }
+  }
+  
+  # weight to decide between the two species 
+  print(weight)
+  return(errors)
+}
+
+err <- perceptron(x,y,1,10)
+
+plot(1:10, err, type="l", lwd=2, col="red", xlab="epoch #", ylab="errors")
+title("Errors vs epoch - learning rate eta = 1")
+
+
+
+
+#Hyperplane for iris-viginica versus iris-setosa OR iris-vesicolor
+
+iris_subdf2 <- iris[, c(1, 3, 5)]
+names(iris_subdf2) <- c("sepal", "petal", "species")
+
+#Data visualization using ggplot2
+
+ggplot(iris_subdf2, aes(x = sepal, y = petal)) + 
+  geom_point(aes(colour=species, shape=species), size = 3) +
+  xlab("sepal length") + 
+  ylab("petal length") + 
+  ggtitle("Species vs sepal and petal lengths") #Doesn't seem to be linearly separable
+
+
+#Keeping all the attributes
+
+x <- iris[, 1:4] 
+names(x) <- tolower(names(x))
+
+# create species labels
+y <- rep(-1, dim(x)[1])     #others are -1
+y[iris[, 5] == "Iris-virginica"] <- 1    #Virginica is 1
+
+# compute and plot error
+err1 <- perceptron(x, y, 0.01, 50)
+
+plot(1:50, err1, type="l", lwd=2, col="red", xlab="epoch #", ylab="errors")
+title("Errors in differentiating Virginica vs epoch - learning rate eta = 0.01") #Minimum error is 2, but the weight converged
 
 
 
